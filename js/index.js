68d076217c73813e9a132bf036b507d49675100a
Shamrin_dev <dev.shamrin@gmail.com>
2024-06-23 15:01:10 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,39 @@
+"usr\er striсt";
+
+// const result = confirm('Are you yere?');
+// console.log(result);
+
+// const answer = +prompt("Are you 18 ears?", "18");
+// console.log(answer * 2);
+
+// const answers = [];
+
+// answers[0] = prompt('Name&', '' );
+// answers[1] = prompt('Familiya', '' );
+// answers[2] = prompt('Age&', '' );
+
+// document.write(answers);
+
+// const category = 'toys';
+
+// console.log(`https://leetcode.com/${category}/5`);
+
+let incr = 10;
+let decr = 10;
+
+// incr++;
+// decr--;
+
+console.log(incr);
+console.log(decr);
+
+
+console.log(10%5);
+
+console.log(3 * 4 == 12);
+
+
+const isChecket = true,
+      isClose = false;
+
+console.log(isChecket || isClose);     
